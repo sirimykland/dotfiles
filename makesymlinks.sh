2ffb844a58778e903a9455a705eb1e0c0d6f908d
Mykland, Siri <Siri.Mykland@skatteetaten.no>
2021-09-08 12:10:59 +0200

    new make symlink script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,7 +1,32 @@
 #!/bin/sh
 
-for FILE in .bashrc .bash_aliases .bash_local .vimrc .vim .git-completion.bash; 
-do 
-    ln -sfv ~/.dotfiles/$FILE ~/$FILE ; 
-done
+if [[ $* = "help" ]]; then
+    echo "To use this script:
+        ./makesymlinks [zsh|bash|other]"
 
+elif [[ $* =~ "zsh" ]]; then
+    echo Symlinking zsh...
+    for FILE in .zshrc .zsh_aliases; 
+    do 
+        ln -sfv ~/.dotfiles/$FILE ~/$FILE ; 
+    done
+    ln -sv ~/.dotfiles/.zsh_local ~/.zsh_local ; 
+
+elif [[ $* =~ "bash" ]]; then
+    echo Symlinking bash...
+    for FILE in .bashrc .bash_aliases; 
+    do 
+        ln -sfv ~/.dotfiles/$FILE ~/$FILE ; 
+    done
+    ln -sv ~/.dotfiles/.bash_local ~/.bash_local 
+
+elif [[ $1 =~ "other" ]]; then
+    echo Symlinking other files...
+    for FILE in .vimrc .vim .git-completion.bash; 
+    do 
+        ln -sfv ~/.dotfiles/$FILE ~/$FILE ; 
+    done
+else 
+    echo "To use this script:
+        ./makesymlinks [zsh|bash|other]" 
+fi
