af15cf735fcda477d44131f4ef718aa64d45d9db
Siri Mykland <sirim_mykland@hotmail.com>
2021-09-13 18:48:19 +0200

    changed symlinks naming, aan zsh path

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,24 +8,24 @@ elif [[ $* =~ "zsh" ]]; then
     echo Symlinking zsh...
     for FILE in .zshrc .zsh_aliases; 
     do 
-        ln -sfv ~/.dotfiles/$FILE ~/$FILE ; 
+        ln -sfv ~/.dotfiles/$FILE ~/$FILE; 
     done
-    ln -sv ~/.dotfiles/.zsh_local ~/.zsh_local ; 
-    git update-index --skip-worktree .zsh_local ;
+    ln -sv ~/.dotfiles/.zsh_local ~/.zsh_local; 
+    git update-index --skip-worktree .zsh_local;
 elif [[ $* =~ "bash" ]]; then
     echo Symlinking bash...
     for FILE in .bashrc .bash_aliases; 
     do 
         ln -sfv ~/.dotfiles/$FILE ~/$FILE ; 
     done
-    ln -sv ~/.dotfiles/.bash_local ~/.bash_local 
-    git update-index --skip-worktree .bash_local
+    ln -sv ~/.dotfiles/.bash_local ~/.bash_local; 
+    git update-index --skip-worktree .bash_local;
 
 elif [[ $1 =~ "other" ]]; then
     echo Symlinking other files...
     for FILE in .vimrc .vim .git-completion.bash; 
     do 
-        ln -sfv ~/.dotfiles/$FILE ~/$FILE ; 
+        ln -sfv ~/.dotfiles/$FILE ~/$FILE; 
     done
 else 
     echo "To use this script:
