8abc6b288215e128119499a4fc5e37577cbf9b28
Siri Mykland <siri.mykland@skattetaten.no>
2021-10-13 13:51:06 +0200

    added tmux config

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -20,7 +20,9 @@ elif [[ $* =~ "bash" ]]; then
     done
     ln -sv ~/.dotfiles/.bash_local ~/.bash_local; 
     git update-index --skip-worktree .bash_local;
-
+elif [[ $* =~ "tmux" ]]; then
+    echo Symlinking tmux config...
+        ln -sfv ~/.dotfiles/.tmux.conf ~/.tmux.conf ; 
 elif [[ $1 =~ "other" ]]; then
     echo Symlinking other files...
     for FILE in .vimrc .vim .git-completion.bash; 
